86205b5081bccbb2ad9694080c8d519d0b742fb9
franciscodara <franciscodara@gmail.com>
2021-04-29 15:13:01 -0300

    Routes & pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,22 @@
-import format from 'date-fns/format';
-import ptBR from 'date-fns/locale/pt-BR';
-import styles from './styles.module.scss';
+import format from "date-fns/format";
+import ptBR from "date-fns/locale/pt-BR";
+import Link from "next/link";
+import styles from "./styles.module.scss";
 
-export default function Header () {
-    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
-        locale: ptBR
-    });
+export default function Header() {
+  const currentDate = format(new Date(), "EEEEEE, d MMMM", {
+    locale: ptBR,
+  });
 
-    return (
-        <header className={styles.headerContainer}>
-            <img src="logo.svg" alt="logo-daracast"/>
-            <p>Aqui a pauta é livre!</p>
-            <span>{currentDate}</span>
-        </header>
-    );
+  return (
+    <header className={styles.headerContainer}>
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="logo-daracast" />
+        </a>
+      </Link>
+      <p>Aqui a pauta é livre!</p>
+      <span>{currentDate}</span>
+    </header>
+  );
 }
